099e5f3e9691613f8efb48c2fe17e4939cb65ace
злата <118795314+dzhoshua@users.noreply.github.com>
2023-10-21 22:12:07 +0800

    Create main.py

---
+++ chain_algorithm/main.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import label
+
+def neighbours4(y, x):
+    return (y, x+1), (y, x-1), (y-1, x), (y+1, x)
+
+def neighbours8(y, x):
+     return neighbours4(y, x) + ((y-1, x+1), (y+1, x+1), (y-1, x-1), (y+1, x-1))
+
+def get_boundaries(labeled, label=1, connectivity=neighbours4):
+    pos = np.where(labeled == label)
+    bounds = []
+    for y, x in zip(*pos):
+        for yn, xn in connectivity(y, x):
+            if yn < 0 or yn > labeled.shape[0]-1:
+                bounds.append((y, x))
+                break
+            elif xn < 0 or xn > labeled.shape[1]-1: 
+                bounds.append((y, x))
+                break
+            elif labeled[yn, xn] == 0:
+                bounds.append((y, x))
+                break
+    return bounds
+
+def directions(y, x):
+  dirs = {
+      (y, x + 1): 0,
+      (y + 1, x + 1): 1,
+      (y + 1, x): 2,
+      (y + 1, x - 1): 3,
+      (y, x - 1): 4,
+      (y - 1, x - 1): 5,
+      (y - 1, x): 6,
+      (y - 1, x + 1): 7
+      }
+  return dirs
+
+
+def chain(labeled, lbl):
+    bounds = get_boundaries(labeled, lbl)
+    chain = []
+    start = bounds[0]
+    point = bounds[1]
+    while point != start:
+        bounds.pop(bounds.index(point))
+        dirs = directions(point[0], point[1])
+        for direction in dirs.keys():
+            if direction in bounds:
+                chain.append(dirs[direction])
+                point = direction
+                break
+    chain.append(0)
+    return chain
+
+
+img = np.array([
+    [0,0,0,0],
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0]
+])
+labeled = label(img)
+print(get_boundaries(labeled))
+print(chain(labeled, 1))
+plt.imshow(labeled)
+plt.show()
+
+
+image = np.load("similar.npy")
+labeled = label(image)
+for i in range(1, labeled.max() + 1):
+  #print(get_boundaries(labeled))
+  print(f"Figure{i}:", chain(labeled, i))
+
+plt.imshow(labeled)
+plt.show()
