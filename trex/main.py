f9044d2c7e8a7f7729536d9eb37b5aed34dc5457
злата <118795314+dzhoshua@users.noreply.github.com>
2023-12-16 00:18:30 +0800

    Update main.py

--- trex/main.py
+++ trex/main.py
@@ -7,43 +7,41 @@ import time
 
 #game_screen = {"top": 330, "left": 130,"width": 800, "height": 90}
 offset_screen = {"top": 350, "left": 196, "width": 65, "height": 45}
-upper_bird_screen = {"top": 325, "left": 203, "width": 70, "height": 26}
+upper_bird_screen = {"top": 325, "left": 203, "width": 75, "height": 26}
 
-start = time.time()
 after_up_sleep = 0.16
-bird_sleep = 0.2
+bird_sleep = 0.3
 val = 5
-speed = 0.001
+speed = 0.002
 
 def get_gray_image(screen):
     img = np.array(sct.grab(screen))
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
     ret, img = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
     return img
-
+start = time.time()
 with mss.mss() as sct:
     while True:
         diff = time.time() - start
-        if diff >= 10:
+        if diff >= 15:
             start = time.time()
+
+            if after_up_sleep <= 0.158:
+                speed = 0.008
+                val = 10
+            if 0.11 < after_up_sleep <= 0.145:
+                val = 15
+            if after_up_sleep<= 0.11:
+                speed = 0
+                val += 1
+
             after_up_sleep -= speed
+            bird_sleep += 0.005
             offset_screen["width"] += val
             upper_bird_screen["width"] += val
             print("sleep", after_up_sleep)
             print("offset", offset_screen["width"])
             print("bird", upper_bird_screen["width"])
-
-
-        if after_up_sleep <= 0.155:
-            speed = 0.005
-            val = 10
-
-        if 0 < after_up_sleep <= 0.14:
-            speed = 0.01
-            val = 20
-        if after_up_sleep - speed <= 0:
-            speed = 0
-            after_up_sleep = 0
             
         
         offset = get_gray_image(offset_screen)
